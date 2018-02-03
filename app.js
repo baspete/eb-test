9243f6cb340a67ea933cf233ca388a240718816f
Pete Butler <pete.butler@ge.com>
2018-02-02 16:58:49 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+const express = require('express'),
+	serveStatic = require('serve-static');
+
+const app = express();
+
+// ========================================================================
+// STATIC ASSETS
+// serveStatic() here will cache these static assets in memory so we don't
+// read them from the filesystem for each request.
+// http://expressjs.com/en/advanced/best-practice-performance.html
+app.use('/', serveStatic('build'));
+
+// ========================================================================
+// START THE SERVER
+const port = process.env.PORT || 3000;
+app.listen(port);
