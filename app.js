98b92e4cd836bdc64f89810143c0773e480063fc
Pete Butler <pete.butler@ge.com>
2018-02-06 11:48:45 -0800

    putting react app in client/

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 // serveStatic() here will cache these static assets in memory so we don't
 // read them from the filesystem for each request.
 // http://expressjs.com/en/advanced/best-practice-performance.html
-app.use('/', serveStatic('build'));
+app.use('/', serveStatic('client/build'));
 
 // ========================================================================
 // START THE SERVER
